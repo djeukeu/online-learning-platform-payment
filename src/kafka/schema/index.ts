9bddbe0b339ba7e97a262310a3d9e09baab4cb8c
djeukeu <dmxchristian@gmail.com>
2025-07-27 21:33:17 +0100

    fix: update kafka configuration

--- src/kafka/schema/index.ts
+++ src/kafka/schema/index.ts
@@ -1,12 +1,12 @@
-export const enrollSchema = `
-{
-    "type": "record",
-    "name": "Enroll",
-    "namespace": "com.online",
-    "fields": [
-        { "name": "user_id", "type": "string", "default": "" },
-        { "name": "course_id", "type": "string", "default": "" },
-        { "name": "payment_id", "type": "string", "default": "" }
-    ]
-}
-`;
+const enroll = {
+    type: 'record',
+    name: 'Enroll',
+    namespace: 'com.online',
+    fields: [
+        { name: 'user_id', type: 'string', default: '' },
+        { name: 'course_id', type: 'string', default: '' },
+        { name: 'payment_id', type: 'string', default: '' },
+    ],
+};
+
+export const enrollSchema = JSON.stringify(enroll);
